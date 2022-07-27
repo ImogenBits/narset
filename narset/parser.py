fb3212964f4aa9ca0127d8bea90648359e9a78f4
Imogen <me@imogen.tech>
2022-07-28 00:36:49 +0200

    basic tokenizer

--- narset/parser.py
+++ narset/parser.py
@@ -1,21 +1,47 @@
 from __future__ import annotations
-from typing import Iterable
+import nltk
 
 class Token:
     pass
 
-class Lit(Token):
+class Word(Token):
     def __init__(self, s: str) -> None:
         if len(s) != 1:
             raise ValueError
         super().__init__()
         self.s = s
-    
-    def tokens(self, s: str) -> Iterable[Lit]:
-        for c in s:
-            yield Lit(c)
+
 
 class Num(Token):
     def __init__(self, num: int) -> None:
         super().__init__()
         self.num = num
+
+
+class Sym(Token):
+    symbols = {".", ",", "-", "—"}
+
+    def __init__(self, sym) -> None:
+        super().__init__()
+        self.sym = sym
+
+EM = Sym("—")
+
+
+
+
+
+
+def tokenize(text: str) -> list[list[Token]]:
+    out: list[list[Token]] = []
+    for line in text.splitlines():
+        line_out = []
+        out.append(line_out)
+        for word in nltk.word_tokenize(line):
+            if word.isdigit():
+                line_out.append(Num(int(word)))
+            elif word in Sym.symbols:
+                line_out.append(Sym(word))
+            else:
+                line_out.append(Word(word))
+    return out
